bb8a0a7a46bb5165cd4f1875e9ae289ea7b3247c
karan100010 <karanjoshi1993@gmail.com>
2024-01-03 09:12:55 +0530

    added noise dedection

--- example_application.py
+++ example_application.py
@@ -3,16 +3,45 @@
 from time import sleep
 # Import everything from the audiosocket module
 from audiosocket import *
-from asterisk.agi import *
 from pydub import AudioSegment
 from pydub.utils import make_chunks
-
+import numpy as np
+import sounddevice as sd
+import webrtcvad 
 import wave
 
+# Parameters for audio streaming
+channels = 1
+sample_rate = 8000  # You may need to adjust this based on your audio source
+
+# Parameters for VAD
+vad = webrtcvad.Vad()
+vad.set_mode(3)  # Aggressiveness mode (0 to 3)
+
+# Variables for noise detection
+noise_frames_threshold = int(2 * sample_rate / 512)  # 2 seconds
+noise_frames_count = 0
+
+#read a wav file 
 def read_wave_file(filename):
     with wave.open(filename, 'rb') as wave_file:
         audio = wave_file.readframes(wave_file.getnframes())
     return audio
+
+# Function to detect noise
+def detect_noise(indata, frames,rate):
+    global noise_frames_count
+
+    # Convert the audio data to 16-bit integers
+    samples = np.frombuffer(indata, dtype=np.int16)
+    
+    # Perform noise detection using VAD
+    is_noise = vad.is_speech(samples.tobytes(), rate)
+
+    if is_noise:
+        noise_frames_count += frames
+
+
 
 
 # Create a new Audiosocket instance, passing it binding
@@ -47,6 +76,10 @@ for i in  range(int(len(myaudio)/320)):
 
 while conn.connected:
   audio_data = conn.read()
+  # Detect noise
+  detect_noise(audio_data, 1, 8000)
+  print(noise_frames_count)
+   
   #read a wav file from the system and convert it to ulaw
   
 
