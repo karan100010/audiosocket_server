907f2fd9aff4c8b73ffb16de684fe08cf569762c
karan100010 <karanjoshi1993@gmail.com>
2024-01-07 08:03:54 +0530

    changed audio function into a class

--- example_application.py
+++ example_application.py
@@ -8,104 +8,84 @@ from mylogging import ColouredLogger
 import wave
 import threading
 import sys
-# Parameters for audio streaming
-logger=ColouredLogger("audio sharing")
-channels = 1
-sample_rate = 8000  # You may need to adjust this based on your audio source
+import requests
+from mapping import *
 
-# Parameters for VAD
-vad = webrtcvad.Vad()
-vad.set_mode(3)  # Aggressiveness mode (0 to 3)
+class AudioStreamer:
+  def __init__(self):
+    self.logger = ColouredLogger("audio sharing")
+    self.channels = 1
+    self.sample_rate = 8000
+    self.vad = webrtcvad.Vad()
+    self.vad.set_mode(3)
+    self.noise_frames_threshold = int(2 * self.sample_rate / 512)
+    self.noise_frames_count = 0
+    self.audiosocket = Audiosocket(("localhost", 1122))
+    self.conn = None
+    self.w = 0
+    self.v = 320
+    self.level = 1
 
-# Variables for noise detection
-noise_frames_threshold = int(2 * sample_rate / 512)  # 2 seconds
-noise_frames_count = 0
-
-# fuctions 
-
-
-def send_audio(audio_file,logger):
-  global w
-  global v
-  global noise_frames_count
-  for i in range(int(len(audio_file)/320)):
-      
-      
-      conn.write(audio_file[w:v])
-      w+=320
-      v+=320
-      logger.DEBUG("Sending audio")
-      sleep(.005)
-      detect_noise(audio_data, 1, 8000)
-      #if i is devisable by 5 enter sleep for .1 seconds
-      if noise_frames_count>10:
-        sys.exit()
-
-#read a wav file 
-def read_wave_file(filename):
+  def read_wave_file(self, filename):
+    self.logger.debug("Reading wave file")
     with wave.open(filename, 'rb') as wave_file:
-        audio = wave_file.readframes(wave_file.getnframes())
+      audio = wave_file.readframes(wave_file.getnframes())
     return audio
 
-# Function to detect noise
-def detect_noise(indata, frames,rate):
-    global noise_frames_count
-
-    # Convert the audio data to 16-bit integers
+  def detect_noise(self, indata, frames, rate):
+    self.logger.debug("Detecting noise")
     samples = np.frombuffer(indata, dtype=np.int16)
-    
-    # Perform noise detection using VAD
-    is_noise = vad.is_speech(samples.tobytes(), rate)
-
+    is_noise = self.vad.is_speech(samples.tobytes(), rate)
     if is_noise:
-        noise_frames_count += frames
+      self.logger.debug("Noise detected")
+      self.noise_frames_count += frames
 
-#end functions
+  def send_audio(self, audio_file, url, port, service=""):
+    for i in range(int(len(audio_file) / 320)):
+      self.conn.write(audio_file[self.w:self.v])
+      self.w += 320
+      self.v += 320
+      self.logger.debug("Sending audio")
+      sleep(.005)
+      self.detect_noise(audio_data, 1, 8000)
+      if self.noise_frames_count > 4:
+        self.logger.debug("Noise detected")
+        file = self.read_wave_file(mapping[4])
+        j = 0
+        k=320
+        for i in range(int(len(file) / 320)):
+          self.conn.write(file[self.w:self.v])
+          self.j += 320
+          self.k += 320
+          self.logger.debug("Sending audio")
+          sleep(.005)
+        self.level = 4
+        sys.exit()
+      else:
+        self.level += 1
 
+  def start_streaming(self,mapping):
+    self.audiosocket = Audiosocket(("localhost", 1122))
+    self.conn = self.audiosocket.listen()
+    print('Received connection from {0}'.format(self.conn.peer_addr))
+    audio_file = "../output.wav"
+    myaudio = self.read_wave_file(audio_file)
+    process = threading.Thread(target=self.send_audio, args=(myaudio,))
+    process.start()
+    while self.conn.connected:
+      audio_data = self.conn.read()
+      if self.level == 1:
+        process = threading.Thread(target=self.send_audio, args=(mapping[1],))
+        process.start()
+      if self.level == 2:
+        process = threading.Thread(target=self.send_audio, args=(mapping[2],))
+        process.start()
+        val = 0
+      if self.level == 3:
+        process = threading.Thread(target=self.send_audio, args=(mapping[3],))
+        process.start()
+        val = 0
+    print('Connection with {0} over'.format(self.conn.peer_addr))
 
-
-# Create a new Audiosocket instance, passing it binding
-# information in a tuple just as you would a raw socket
-audiosocket = Audiosocket(("localhost", 1122))
-
-# This will block until a connection is received, returning
-# a connection object when one occurs
-conn = audiosocket.listen()
-
-
-print('Received connection from {0}'.format(conn.peer_addr))
-
-
-audio_file="../output.wav"
-
-  
-
-myaudio=read_wave_file(audio_file)
-#splitting the audio file into chunks of 16-bit, 8kHz, mono PCM
-w=0
-v=320
-
-process=threading.Thread(target=send_audio,args=(myaudio,))
-
-
-
-  
-   
-
-
-level=1
-while conn.connected:
-  audio_data = conn.read()
-  # Detect noise
-  
-  print(noise_frames_count)
-  if val==1:
-     process.start()
-     val=0
-
-  
-  
-  
-
-
-print('Connection with {0} over'.format(conn.peer_addr))
+streamer=AudioStreamer()
+streamer.start_streaming(mapping)
