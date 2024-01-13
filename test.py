33a7e02a1272b9721cf2c16b7d3d26f998bc140a
karan100010 <karanjoshi1993@gmail.com>
2024-01-13 15:49:40 +0530

    commnted out app

--- test.py
+++ test.py
@@ -14,8 +14,8 @@ import json
 import base64
 from example_application import AudioStreamer
 
-stream=AudioStreamer()
-while stream.conn.connected:
-    data=stream.conn.read()
-    req={"audiofile":base64.b64encode(data).decode('utf-8')}
-    requests.post("http://localhost:5000/audio",json=req)
+# stream=AudioStreamer()
+# while stream.conn.connected:
+#     data=stream.conn.read()
+#     req={"audiofile":base64.b64encode(data).decode('utf-8')}
+#     requests.post("http://localhost:5000/audio",json=req)
