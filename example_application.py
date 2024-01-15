eb6bcd54be472e1054523b6c82512884299a18c6
karan100010 <karanjoshi1993@gmail.com>
2024-01-16 02:36:42 +0530

    added

--- example_application.py
+++ example_application.py
@@ -117,7 +117,7 @@ class AudioStreamer:
           
           x = self.read_wave_file(mapping["en"][1])
           self.send_audio(call,x)
-          self.logger.info("audio length is "+str(self.read_length(mapping[self.channel][self.level])) + " seconds")
+          #self.logger.info("audio length is "+str(self.read_length(mapping[self.channel][self.level])) + " seconds")
 
           self.audioplayback=False
           sleep(1)
