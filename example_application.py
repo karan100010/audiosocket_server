77586aa6490957decbfe68ebb4fae69be1628dbf
karan100010 <karanjoshi1993@gmail.com>
2024-01-16 13:10:38 +0530

    added

--- example_application.py
+++ example_application.py
@@ -107,7 +107,7 @@ class AudioStreamer():
       else:
         self.combined_audio+=audio_data
         self.dedect_silence(audio_data,1,8000)
-       # self.logger.info("silence detection started the value of silent fames is {}".format(self.silent_frames_count))  
+        self.logger.info("silence detection started the value of silent fames is {}".format(self.silent_frames_count))  
     return
   def start_audio_playback(self,mapping):
     self.logger.info('Received connection from {0}'.format(self.call.peer_addr))
