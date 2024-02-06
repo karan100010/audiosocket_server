34d6ca33fcb0cfdc6f7017a0bc4c38a3e16e3b59
karan100010 <karanjoshi1993@gmail.com>
2024-02-06 17:49:37 +0530

    added

--- example_application.py
+++ example_application.py
@@ -154,7 +154,8 @@ class AudioStreamer():
             while self.cotinues_silence_normal<75:
                 sleep(.01)
             response=requests.post("http://3.108.66.52:5002/convert",data=self.combined_audio)
-            if dict(response.text)["transcribe"]=="":
+            resp=json.loads(response.text)
+            if resp["transcribe"]=="":
                 self.level="inturruption"
             self.combined_audio=b''
             print(response.text)
