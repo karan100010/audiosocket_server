bd1bbde60343eebcccd8c9eac408702a6ae2d0ac
karan100010 <karanjoshi1993@gmail.com>
2024-02-06 13:38:32 +0530

    added

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ import os
 #     req={"audiofile":base64.b64encode(data).decode('utf-8')}
 #     requests.post("http://localhost:5000/audio",json=req)
 
-def text_to_speech(text):
+def text_to_speech(text,path):
     # Convert text to speech using a text-to-speech library
     # Store the speech in a byte-sized object with ulaw encoding
     # Return the byte-sized object
@@ -32,7 +32,7 @@ def text_to_speech(text):
     # Create a byte-sized object to store the speech
     
     # Save the speech to the byte-sized object
-    engine.save_to_file(text, "temp.wav")
+    engine.save_to_file(text, path)
     
 
     
@@ -44,7 +44,7 @@ def text_to_speech(text):
 for language in nlp_mapping:
     for key in nlp_mapping[language]:
   
-        speech=text_to_speech(nlp_mapping[language][key])
+       
         if not os.path.exists("demo_audios/"+language+"/"+str(key)+".wav"):
-          with open("demo_audios/"+language+"/"+str(key)+".wav","wb") as file:
-              file.write(speech)
+            text_to_speech(nlp_mapping[language][key],"demo_audios/"+language+"/"+str(key)+".wav")
+            print("demo_audios/"+language+"/"+str(key)+".wav")
