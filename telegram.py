bf13c9a2c45af0ace92048b7cfcf4d0776ee8ce4
karan100010 <karanjoshi1993@gmail.com>
2024-03-01 03:16:23 +0530

    added

--- telegram.py
+++ telegram.py
@@ -76,6 +76,61 @@ def handle_audio(update):
 start_index=0
 end_index=5
 #write a handeler for menu options
+
+@bot.message_handler(content_types=['voice'])
+def handle_audio(update):
+    #dounload the audio file from the user and save it in the server as wav file
+    file = bot.get_file(update.voice.file_id)
+    file_url = f"https://api.telegram.org/file/bot{bot.token}/{file.file_path}"
+    response = requests.get(file_url)
+    print(response.status_code)
+
+    if response.status_code == 200:
+
+       
+# Assuming you have the OGG stream stored in a variable named ogg_stream
+
+        # Create a BytesIO object to treat the stream as a file-like object
+        ogg_stream_bytesio = BytesIO(response.content)
+
+        # Load the OGG audio stream from the BytesIO object
+        ogg_audio = AudioSegment.from_file(ogg_stream_bytesio, format="ogg")
+
+        # Set the desired sample rate (8000Hz)
+        desired_sample_rate = 8000
+
+        # Resample the audio to 8000Hz
+        ogg_audio = ogg_audio.set_frame_rate(desired_sample_rate).set_sample_width(2)
+
+        # Export the audio as a WAV file
+        ogg_audio.export("output.wav", format="wav")
+
+    #merge audio file with header.wav file
+    sound1 = AudioSegment.from_file("output.wav")
+    sound2 = AudioSegment.from_file("demo_audios/en/header.wav")
+    combined = sound2 + sound1
+    combined = combined.set_frame_rate(desired_sample_rate).set_sample_width(2)
+
+    
+    combined.export("final.wav", format='wav')
+    #chmod final.wav file to 777
+    os.system("chmod 777 final.wav")
+
+   
+    manager.originate(
+        channel="SIP/zoiper",
+        context="my-phones",
+        exten="500",  # Assuming Zoiper is extension 100
+        priority=1,
+        caller_id="CallerID <caller_id_number>",
+        timeout=30000,  # Timeout in milliseconds
+        #async=True  # Perform asynchronously
+        application="Playback",
+        data="/home/vboxuser/audiosocket_server/final"
+        
+    )
+    #play audio file for the user when the call is answered
+
 @bot.message_handler(commands=['get_all'])
 def handle_menu(message):
     #send the user a list of menu options
@@ -108,6 +163,7 @@ def handle_menu(message):
     bot.send_message(message.chat.id,"next",reply_markup=reply_markup)
     start_index+=5
     end_index+=5
+
 
 @bot.callback_query_handler(func=lambda call: call.data == 'reply')
 def handle_reply(call):
