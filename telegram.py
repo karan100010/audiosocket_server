a7f0bc22e02db08a66d0dad2d9cd6fb3b6e63fef
karan100010 <karanjoshi1993@gmail.com>
2024-03-01 01:08:29 +0530

    added

--- telegram.py
+++ telegram.py
@@ -6,13 +6,15 @@ import threading
 import requests
 from pydub import AudioSegment
 from io import BytesIO
-
+import pymongo
+from telebot import types
 
 manager = Manager()
 manager.connect('localhost')
 manager.login('karan', 'test')
 chat_id=[]
 # Initialize Telegram bot
+conn= pymongo.MongoClient("mongodb+srv://root:toor@testcluster.exl8ah5.mongodb.net/Grievance?retryWrites=true&w=majority&appName=TestCluster",uuidRepresentation='standard')
 bot = telebot.TeleBot("7144846540:AAGMzRZRmlV8NtQQfQ67vD5butARXFL4tCM")
 @bot.message_handler(commands=['start'])
 def handle_all_messages(message):
@@ -49,11 +51,84 @@ def handle_audio(update):
 
         # Export the audio as a WAV file
         ogg_audio.export("output.wav", format="wav")
+
 #write a handeler for menu options
 @bot.message_handler(commands=['menu'])
 def handle_menu(message):
     #send the user a list of menu options
-    bot.send_message(message.chat.id,"1.List Complain\n")
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    item1 = types.KeyboardButton("Get grivance by category")
+    item2 = types.KeyboardButton("get grivance by language")
+    
+    item3 = types.KeyboardButton("get grivance by date")
+    item4 = types.KeyboardButton("get grivance by status")
+    markup.add(item1, item2, item3,item4)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "Get grivance by category":
+        x=conn["Grievance"]["grievance"].findall()
+        x=x.distinct("category")
+        for i in x:
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(text=i,callback_data=i))
+        bot.send_message(call.message.chat.id,i,reply_markup=markup)
+    elif call.data == "get grivance by language":
+        x=conn["Grievance"]["grievances"].findall()
+        x=x.distinct("language")
+        for i in x:
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(text=i,callback_data=i))
+        bot.send_message(call.message.chat.id,i,reply_markup=markup)
+    elif call.data == "get grivance by date":
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton(text="Today",callback_data="Today"))
+        markup.add(types.InlineKeyboardButton(text="Yesterday",callback_data="Yesterday"))
+        markup.add(types.InlineKeyboardButton(text="Last 7 days",callback_data="Last 7 days"))
+        markup.add(types.InlineKeyboardButton(text="Last 30 days",callback_data="Last 30 days"))
+        bot.send_message(call.message.chat.id,"Select the date",reply_markup=markup)
+    elif call.data == "get grivance by status":
+        x=conn["Grievance"]["grievances"].findall()
+        x=x.distinct("status")
+        for i in x:
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(text=i,callback_data=i))
+        bot.send_message(call.message.chat.id,i,reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+
+def callback2_query(call):
+    if call.data in ["Today","Yesterday","Last 7 days","Last 30 days"]:
+        x=conn["Grievance"]["grievances"].findall({"date":call.data})
+        for i in x:
+            bot.send_message(call.message.chat.id,i)
+    else:
+        x=conn["Grievance"]["grievances"].findall({"status":call.data})
+        for i in x:
+            bot.send_message(call.message.chat.id,i)
+@bot.callback_query_handler(func=lambda call: True)
+def callback3_query(call):
+    x=conn["Grievance"]["grievances"].findall({"category":call.data})
+    for i in x:
+        bot.send_message(call.message.chat.id,i)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback4_query(call):
+    x=conn["Grievance"]["grievances"].findall({"language":call.data})
+    for i in x:
+        bot.send_message(call.message.chat.id,i)
+
+
+
+
+    
+    
+       
+
+
+
+    
+        
+
     
 
 #write a handeler for menu options
