89a25ce2bb2c8affadda16142a818de3f9c228a4
karan100010 <karanjoshi1993@gmail.com>
2024-03-03 21:34:49 +0530

    added

--- mapping.py
+++ mapping.py
@@ -1,11 +1,11 @@
 mapping={
         "en":[{"welcome":["demo_audios/en/rec1_eng.wav"],
-               "positive":["demo_audios/en/positive/rec5_eng.wav","demo_audios/en/positive/rec8.wav"],
-                "negative":["change_flow","demo_audios/en/negative/rec9_eng.wav"]},
+               "positive":["demo_audios/en/main_flow//positive/rec5_eng.wav","demo_audios/en/main_flow//positive/rec8.wav"],
+                "negative":["change_flow","demo_audios/en/main_flow//negative/rec9_eng.wav"]},
                 
-                {"welcome":["demo_audios/en/rec2_eng.wav"],
-                 "positive":["demo_audios/en/positive/rec3_eng.wav","demo_audios/en/positive/rec3_eng.wav"],
-                 "negative":["demo_audios/en/negative/rec7_eng.wav","demo_audios/en/negative/rec6_eng.wav"]},
+                {"welcome":["demo_audios/en/side_flow/rec2_eng.wav"],
+                 "positive":["demo_audios/en/side_flow/positive/rec3_eng.wav","demo_audios/en/side_flow/positive/rec3_eng.wav"],
+                 "negative":["demo_audios/en/side_flow/negative/rec7_eng.wav","demo_audios/en/negative/rec6_eng.wav"]},
                  ],
 
          
