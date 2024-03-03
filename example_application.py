37e1ea6ef4b7a622278b790e8b479bfd610f2184
karan100010 <karanjoshi1993@gmail.com>
2024-03-03 23:32:19 +0530

    added

--- example_application.py
+++ example_application.py
@@ -196,15 +196,21 @@ class AudioStreamer():
             while self.long_silence<100:
               sleep(.01)
             self.long_silence=0
-            if self.channel=="en":
-              try:
-                response=requests.post("http://172.16.1.209:5002/convert_{}".format(self.channel),data=self.combined_audio)
-                resp=json.loads(response.text)
-                print(resp)
-                self.combined_audio=b''
-           
-              except Exception as e:
-                self.logger.info(e)
+            if self.level==0 and self.intent=="welcome":
+  
+              response=requests.post("http://172.16.1.209:5000/predict",data=self.combined_audio)
+              resp=json.loads(response.text)
+              print(resp)
+            self.channel=resp["prediction"][0]
+        
+            try:
+              response=requests.post("http://172.16.1.209:5002/convert_{}".format(self.channel),data=self.combined_audio)
+              resp=json.loads(response.text)
+              print(resp)
+              self.combined_audio=b''
+          
+            except Exception as e:
+              self.logger.info(e)
               #self.call.hangup()
             # if resp["transcribe"]=="":
             #     self.level="cant_hear"
