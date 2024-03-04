b9ce9a88c5c83658d55150c34e7d053b81cbd3ca
karan100010 <karanjoshi1993@gmail.com>
2024-03-04 10:59:15 +0530

    added

--- mapping.py
+++ mapping.py
@@ -1,18 +1,18 @@
 mapping={
         "en":[{"welcome":["demo_audios/en/rec1_eng.wav"],
-               "positive":["demo_audios/en/main_flow/positive/rec5_eng.wav","demo_audios/en/main_flow/positive/rec8_eng.wav"],
-                "negative":["change_flow","demo_audios/en/main_flow/negative/rec9_eng.wav"]},
+               "positive":["demo_audios/en/main_flow/positive/rec5_eng.wav","demo_audios/en/main_flow/positive/rec8_eng.wav","end_call"],
+                "negative":["change_flow","demo_audios/en/main_flow/negative/rec9_eng.wav","end_call"]},
                 
                 {"welcome":["demo_audios/en/side_flow/rec2_eng.wav"],
-                 "positive":["demo_audios/en/side_flow/positive/rec2_eng.wav","demo_audios/en/side_flow/positive/rec3_eng.wav"],
-                 "negative":["demo_audios/en/side_flow/negative/rec6_eng.wav","demo_audios/en/side_flow/negative/rec7_eng.wav"]},
+                 "positive":["demo_audios/en/side_flow/positive/rec2_eng.wav","demo_audios/en/side_flow/positive/rec3_eng.wav","switch_flow"],
+                 "negative":["demo_audios/en/side_flow/negative/rec6_eng.wav","demo_audios/en/side_flow/negative/rec7_eng.wav","end_call"]},
                  ],
 
          
 
          "hi":[{"welcome":["demo_audios/hi/rec1_hin.wav"],
-               "positive":["demo_audios/hi/main_flow/positive/rec5_hin.wav","demo_audios/hi/main_flow/positive/rec8_hin.wav"],
-                "negative":["change_flow","demo_audios/hi/main_flow/negative/rec9_hin.wav"]},
+               "positive":["demo_audios/hi/main_flow/positive/rec5_hin.wav","demo_audios/hi/main_flow/positive/rec8_hin.wav","end_call"],
+                "negative":["change_flow","demo_audios/hi/main_flow/negative/rec9_hin.wav","end_call"]},
                     
                     {"welcome":["demo_audios/hi/side_flow/rec2_hin.wav"],
                     "positive":["demo_audios/hi/side_flow/positive/rec3_hin.wav","demo_audios/hi/side_flow/positive/rec3_hin.wav"],
