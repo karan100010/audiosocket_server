40a8aef44102117c8e58672f1591a0d307d808ec
karan100010 <karanjoshi1993@gmail.com>
2024-03-17 15:03:25 +0530

    added

--- call.py
+++ call.py
@@ -1,15 +1,2 @@
-from langdetect import detect
 
-
-def is_language(text):
-    try:
-        detect(text)
-        return True
-    except:
-        return False
-        def is_english(text):
-            try:
-                lang = detect(text)
-                return lang == 'en'
-            except:
-                return False
+x.register()
