a025317fda27c8162dc97e88773ec896b2ba2aad
karan100010 <karanjoshi1993@gmail.com>
2024-03-26 02:31:20 +0530

    added

--- server_test.py
+++ server_test.py
@@ -39,7 +39,7 @@ for i in range(iters):
 for thread in threads:
     if thread:
         thread.join()
-if len(resp_lis)<iters*3:
+if len(resp_lis)!=iters*3:
     time.sleep(1)
     print("waiting for all threads to complete")
 df["response_time"]=resp_lis
