74c7c366acb61970ec21ad26eeb56370bfe81752
karan100010 <karanjoshi1993@gmail.com>
2024-03-26 01:18:47 +0530

    added server_test.py

---
+++ demo_audios/server_test.py
@@ -0,0 +1,33 @@
+import wave
+import os
+import asyncio
+import pandas as pd
+import requests
+import time
+import threading
+def read_wave_file(filename):
+    #self.logger.debug("Reading wave file")
+    with wave.open(filename, 'rb') as wave_file:
+      audio = wave_file.readframes(wave_file.getnframes())
+    return audio
+
+iters= 10 
+def send_file(audio):
+   start_time = time.time()
+   response=requests.post("http://172.16.1.209:5002/convert_en",data=audio)
+   response_time = time.time() - start_time
+   return response_time
+def combined(filename):
+    audio=read_wave_file(filename)
+    response_time=send_file(audio)
+    df=df.append({"response_time":response_time,"file_name":"demo_audios/resp/1.wav","call_num":i},ignore_index=True)
+    return
+   
+df=pd.DataFrame(columns=["response_time","file_name","call_num"])
+for i in range(iters):
+    for i in os.listdir("demo_audios/resp"):
+        audio=read_wave_file()
+        response_time=threading.Thread(target=combined,args=(audio,)).start()
+        time.sleep(1)
+df.to_csv("test.csv")
+print("testing done")
