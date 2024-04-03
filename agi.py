a7e3804f7e7e52ee7bdc35476e6b503450dc8bdc
karan100010 <karanjoshi1993@gmail.com>
2024-04-03 23:11:31 +0530

    added

--- agi.py
+++ agi.py
@@ -10,6 +10,6 @@ agi=astrisk.AGI()
 #start a audio socket server
 # start a audio socket server
 agi.stream_file('demo_audios/resp/1.wav')
-agi.exec_command("Audiosocket",[str(uuid.uuid4()),"localhost:1122"])
-agi.stream_flie("demo_audios/resp/2.wav")
-agi.hangup()
+# agi.exec_command("Audiosocket",[str(uuid.uuid4()),"localhost:1122"])
+# agi.stream_flie("demo_audios/resp/2.wav")
+# agi.hangup()
