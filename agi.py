0e7c05e5fa6cbc35a89d88d3f22f6deca6e7971c
karan100010 <karanjoshi1993@gmail.com>
2024-04-07 12:09:46 +0530

    added

--- agi.py
+++ agi.py
@@ -9,7 +9,7 @@ agi= AGI()
 #start a audio socket server
 # start a audio socket server
 agi.stream_file('/home/vboxuser/audiosocket_server/demo_audios/resp/1')
-agi.exec_command("Dial","(AudioSocket/localhost:1122/${SIP_HEADER(Call-ID)},,g")
+agi.exec_command("Dial","AudioSocket/localhost:1122/${SIP_HEADER(Call-ID)},,g")
 #agi.exec_command("Verbose","audiosocket has ended")
 agi.stream_flie("/home/vboxuser/audiosocket_server/demo_audios/resp/2")
 agi.hangup()
