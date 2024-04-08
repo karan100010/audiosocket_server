4f3f6c447eabd1cdce5e72e340b265aff5d78f39
karan100010 <karanjoshi1993@gmail.com>
2024-04-08 13:42:01 +0530

    added

--- example_application.py
+++ example_application.py
@@ -218,7 +218,7 @@ class AudioStreamer():
             self.logger.info("we are in level {}".format(self.level))
             x = self.read_wave_file(mapping[self.channel][self.call_flow_num][self.intent][self.level])
             self.send_audio(x)
-            return b"0x00"*3
+            self.call.hangup()
 
             if self.intent!="welcome":
                
