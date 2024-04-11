1a7de37f465066ca62ddd6c62c29d53485760b1f
karan100010 <karanjoshi1993@gmail.com>
2024-04-11 23:53:59 +0530

    added

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from time import sleep
 from audiosocket import *
+from threading import Thread
 
 
 socket = Audiosocket(('localhost', 1122))
 call=socket.listen()
-call._process()
+x=Thread(call._process())
+x.start()
 print(call.read())
 print(socket.uuid)
