3561b330588e7aa762c02c6fe2951198164c5458
karan100010 <karanjoshi1993@gmail.com>
2024-04-11 23:56:01 +0530

    addedd

--- test.py
+++ test.py
@@ -5,7 +5,4 @@ from threading import Thread
 
 socket = Audiosocket(('localhost', 1122))
 call=socket.listen()
-x=Thread(call._process())
-x.start()
-print(call.read())
-print(socket.uuid)
+print(call.uuid)
