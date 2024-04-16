b8eea83f06ff2f2dc13ee245e5f1b05e7deffdd1
karan100010 <karanjoshi1993@gmail.com>
2024-04-16 06:27:12 +0530

    added

---
+++ connection_api.py
@@ -0,0 +1,84 @@
+
+from flask import Flask, request, jsonify
+import pymongo
+with open("db.txt") as f:
+       data=f.read()
+if data.endswith("\n"):
+    data=data.strip("\n")
+print(data)
+
+app = Flask(__name__)
+
+try:
+    conn = pymongo.MongoClient(data)
+    print("Connected successfully!!!")
+except:
+    print("Could not connect to MongoDB")
+
+@app.route('/api/connections', methods=['POST'])
+def create_connection():
+    connection = conn['test']['connection']
+    data = request.json
+    try:
+        connection_id = connection.insert_one(data)
+        print("sucessfully inserted connection data")
+    except:
+        return jsonify("Error in inserting data")    
+    return jsonify(str(connection_id.inserted_id))
+@app.route('/api/connections/update', methods=['PUT'])
+def update_connection():
+    connection = conn['test']['connection']
+    data = request.json
+    connection_id = data['connection_id']
+    try:
+        connection.update_one({'_id': connection_id}, {'$set': data})
+        print("sucessfully updated connection data")
+    except:
+        return jsonify("Error in updating data")
+@app.route('/api/connections/delete', methods=['DELETE'])
+def delete_connection():
+    connection = conn['test']['connection']
+    data = request.json
+    connection_id = data['connection_id']
+    try:
+        connection.delete_one({'_id': connection_id})
+        print("sucessfully deleted connection data")
+    except:
+        return jsonify("Error in deleting data")
+@app.route('/api/connections/get_free', methods=['GET'])
+def get_free_connection():
+    connection = conn['test']['connection']
+    #find all the connctions that are active 
+    try:
+        connection_data = connection.find({'status': 'active'})
+        #run minima fuction on the num_connected field
+        min_connections = min(connection_data, key=lambda x:x['num_connected'])
+        return jsonify(min_connections)
+    except:
+        return jsonify("Error in getting data")
+    
+#calls apis start here 
+    
+@app.route('/api/connections/calls', methods=['POST'])
+def add_call():
+    connection = conn['test']["calls"]
+    data = request.json
+    connection_id = data['connection_id']
+    try:
+        connection.insert_one(data)
+        print("sucessfully inserted call data")
+    except:
+        return jsonify("Error in inserting data")
+    return jsonify("sucessfully inserted call data")
+@app.route('/api/connections/calls/<id>', methods=['GET'])
+def get_call(id):
+    connection = conn['test']["calls"]
+    try:
+        connection_data = connection.find({'connection_id': id})
+        return jsonify(list(connection_data))
+    except:
+        return jsonify("Error in getting data")
+
+if __name__ == '__main__':
+    app.run(port=5011, debug=True)    
+
