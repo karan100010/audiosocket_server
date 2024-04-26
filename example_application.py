888e8478238946a6195d11d6597da5bc3309f553
karan100010 <karanjoshi1993@gmail.com>
2024-04-26 17:15:35 +0530

    added

--- example_application.py
+++ example_application.py
@@ -258,8 +258,8 @@ class AudioStreamer():
                 x = audioop.alaw2lin(audio_data, 2)
                 #pcm_data = audioop.ratecv(x, 2, 1, 8000, 8000, None)[0]
                 #pcm_data = audioop.lin2lin(pcm_data, 2, 2)
-                self.send_audio(pcm_data)
-                self.logger.info(pcm_data)
+                self.send_audio(x)
+                self.logger.info(x)
 
                 # self.logger.info("we are in level {}".format(self.level))
 
