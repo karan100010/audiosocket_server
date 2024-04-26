889350efb83e9ca6654e4385d9294ca2fa71c420
karan100010 <karanjoshi1993@gmail.com>
2024-04-26 17:38:18 +0530

    added

--- example_application.py
+++ example_application.py
@@ -256,8 +256,8 @@ class AudioStreamer():
                 audio_data = self.call.read()
                 
                 x = audioop.ulaw2lin(audio_data, 4)
-                pcm_data = audioop.ratecv(x, 2, 1, 8000, 8000, 1)[0]
-                pcm_data = audioop.lin2lin(pcm_data, 2, 2)
+                pcm_data = audioop.ratecv(x, 2, 1, 8000, 8000, None)[0]
+                pcm_data = audioop.lin2lin(pcm_data, 4,2)
                 self.send_audio(pcm_data)
                 self.logger.info(pcm_data)
 
