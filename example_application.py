e4c138263b4b4e8bae57e21507e1ba42ff1f747c
karan100010 <karanjoshi1993@gmail.com>
2024-04-28 16:28:01 +0530

    x

--- example_application.py
+++ example_application.py
@@ -58,7 +58,7 @@ class AudioStreamer():
         #     "http://172.16.1.213:3022/call-records/a91d0293-cfb2-40c0-8248-cbabbf64f770").text
         # self.respdict = json.loads(respdict)
         # self.welcome = self.respdict["data"]["intro_rec"]
-        self.welcome_audio = requests.get(self.welcome).content
+        # self.welcome_audio = requests.get(self.welcome).content
       #  req=requests.post(self.call_api,data={"status":"active","addr":self.audiosocket.addr+":"+str(self.audiosocket.port),"conn":0,"time_updates":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
         #print(req.text)
 
