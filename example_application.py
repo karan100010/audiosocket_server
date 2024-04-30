68209b1bf56282ea79071a2d8a31ca60244d3fba
karan100010 <karanjoshi1993@gmail.com>
2024-04-30 08:48:42 +0530

    added

--- example_application.py
+++ example_application.py
@@ -130,7 +130,7 @@ class AudioStreamer():
             
                 if count % 25 == 0:
                     sleep(.25)
-                    sleep_seconds += .4
+                    sleep_seconds += .3
 
             # if self.level!=11:
             # if not self.noise:
