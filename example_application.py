2877c8e43dcb176b9988c3513c702ee25e6cadc9
karan100010 <karanjoshi1993@gmail.com>
2024-04-30 23:45:59 +0530

    add

--- example_application.py
+++ example_application.py
@@ -284,7 +284,7 @@ class AudioStreamer():
                 self.level+=1
 
                     
-                while self.long_silence<50:
+                while self.long_silence<40:
                 #self.logger.info("waiting for silence")
                     if self.call.connected:
                         sleep(.01)
