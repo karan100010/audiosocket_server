7569491607a15cba58045e6224a336b8111f0b69
karan100010 <karanjoshi1993@gmail.com>
2024-04-30 15:43:33 +0530

    added

--- example_application.py
+++ example_application.py
@@ -118,7 +118,7 @@ class AudioStreamer():
             self.call.write(audio_file[w:v])
             w += 320
             v += 320
-            sleep(.05)
+            sleep(.01)
 
 
             # if self.level!=11:
