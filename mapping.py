40c6f9258a8d3a47dd2e319597bb1307f313c845
karan100010 <karanjoshi1993@gmail.com>
2024-05-05 11:23:37 +0530

    added

--- mapping.py
+++ mapping.py
@@ -28,26 +28,26 @@ testmapping=[1,2,3]
 
 map_mongo={
     "main_audios":{
-        "yes_intent_1":["self.master",{"meta":"next_level"}],
-        "yes_intent_2":["http://172.16.1.207:5005/voice/20221205152448KQMS18_EH-M2.wav",{"meta":"next_level"}],
-        "yes_intent_3":["http://172.16.1.207:5005/voice/20221205152604ZW2OEB_EH-M2.wav",{"meta":"transfer"}],
-        "no_intent_1":[" http://172.16.1.207:8084/payment_link_sent.wav",{"meta":"hangup"}],
-        "no_intent_2":["http://172.16.1.207:5005/voice/20230526112400HKP2VP_EH-M5.wav",{"meta":"hangup"}],
-        "no_intent_3":["http://172.16.1.207:5005/voice/202301021418523IWNWJ_EH-M2.wav",{"meta":"hangup"}],
-        "call_back_later_intent_1":["http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav",{"meta":"hangup"}],
-        "call_back_later_intent_2":["http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav",{"meta":"hangup"}],
-        "call_back_later_intent_3":["http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav",{"meta":"hangup"}],
-        "other_intent_1":["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],
-        "other_intent_2":["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],
-        "other_intent_3":["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],
-        "contact_human_agent_intent_1":["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],
-         "contact_human_agent_intent_1": ["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],
-        "contact_human_agent_intent_1" : ["http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav",{"meta":"transfer"}],          
+        "yes_intent_1": [("self.master", {"meta":"next_level"}),( "http://172.16.207:8084/give_phone", {"meta":"next_level"})],
+        "yes_intent_2": [("http://172.16.1.207:5005/voice/20221205152448KQMS18_EH-M2.wav", {"meta":"next_level"}),( "http://172.16.207:8084/phone_given", {"meta":"switch_flow_to_0"})],
+        "yes_intent_3": [(" http://172.16.1.207:8084/payment_link_sent.wav", {"meta":"hangup"}),],
+        "no_intent_1": [("http://172.16.1.207:8084/is_he_aviable", {"meta":"swich_flow"}),( "http://172.16.207:8084/can_share_alt", {"meta":"hangup"})],
+        "no_intent_2": [("http://172.16.1.207:5005/voice/20230526112400HKP2VP_EH-M5.wav", {"meta":"hangup"})],
+        "no_intent_3": [("http://172.16.1.207:5005/voice/202301021418523IWNWJ_EH-M2.wav", {"meta":"hangup"})],
+        "call_back_later_intent_1": [("http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav", {"meta":"hangup"})],
+        "call_back_later_intent_2": [("http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav", {"meta":"hangup"})],
+        "call_back_later_intent_3": [("http://172.16.1.207:5005/voice/20221205152848SB0SXI_EH-M2.wav", {"meta":"hangup"})],
+        "other_intent_1": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],
+        "other_intent_2": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],
+        "other_intent_3": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],
+        "contact_human_agent_intent_1": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],
+        "contact_human_agent_intent_2": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],
+        "contact_human_agent_intent_3": [("http://172.16.1.207:5005/voice/20221213092638PSKL7I_EH-M5.wav", {"meta":"transfer"})],          
     },
     "utils":{
-        "sorry":"http://172.16.1.207:5005/voice/20221205151448TZ19PK_EH-M2.wav",
-        "bye":"http://172.16.1.207:5005/voice/2022122215292066Q8QZ_EH-M2.wav",
-        "inttrupt":"http://172.16.1.207:8084/inttrupt.wav"
+        "sorry": [("http://172.16.1.207:5005/voice/20221205151448TZ19PK_EH-M2.wav",)],
+        "bye": [("http://172.16.1.207:5005/voice/2022122215292066Q8QZ_EH-M2.wav",)],
+        "inttrupt": [("http://172.16.1.207:8084/inttrupt.wav",)]
     },
-    "end_level":4
+    "end_level": 4
 }
