358ffb76619e134bf7a81bf89eef162a44742a01
karan100010 <karanjoshi1993@gmail.com>
2024-05-06 10:48:07 +0530

    added

--- example_application.py
+++ example_application.py
@@ -54,7 +54,7 @@ class AudioStreamer():
         self.long_silence = 0
         self.intent = "welcome"
         self.call_api = "http://localhost:5011/api/connections"
-        self.call_link="http://172.16.1.213:3022/call-records/a91d0293-cfb2-40c0-8248-cbabbf64f770"
+        self.call_link="http://172.16.1.213:3022/call-records/f8bda0a6-32cd-4ca6-9075-5bc260c7fbd0"
         respdict = requests.get(self.call_link
             ).text
         self.respdict = json.loads(respdict)
