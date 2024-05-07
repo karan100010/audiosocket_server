bc673157c2330638c889b1799bac2466ccff662d
karan100010 <karanjoshi1993@gmail.com>
2024-05-08 01:11:56 +0530

    added

--- example_application.py
+++ example_application.py
@@ -144,7 +144,7 @@ class AudioStreamer():
                 sleep(.5)
                 #sleep_seconds+=.25
             if not self.noise:
-                if self.noise_frames_count >= 40:
+                if self.noise_frames_count >= 30:
                     self.noise=True
                     self.noise_frames_count=0
                     self.audioplayback=False
