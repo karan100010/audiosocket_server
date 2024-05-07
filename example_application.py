738cb6157eaf8045242770ac1129f59565e8d0f6
karan100010 <karanjoshi1993@gmail.com>
2024-05-07 23:14:35 +0530

    added

--- example_application.py
+++ example_application.py
@@ -137,7 +137,7 @@ class AudioStreamer():
             #self.detect_noise(indata, 1, 8000)
             count+=1
             if count%25==0:
-                sleep(.75)
+                sleep(.5)
                 #sleep_seconds+=.25
             if not self.noise:
                 if self.noise_frames_count >= 40:
