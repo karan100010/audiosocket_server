8d65e3ef88d4e38f0386b3ab6c5bf57ed2fba61d
karan100010 <karanjoshi1993@gmail.com>
2024-05-08 10:09:34 +0530

    added

--- example_application.py
+++ example_application.py
@@ -30,7 +30,7 @@ class AudioStreamer():
         self.flow_num=0
         self.sample_rate = 8000
         self.vad = webrtcvad.Vad()
-        self.vad.set_mode(2)
+        self.vad.set_mode(3)
         self.noise_frames_threshold = int(2 * self.sample_rate / 512)
         self.noise_frames_count = 0
         self.audiosocket = socket
