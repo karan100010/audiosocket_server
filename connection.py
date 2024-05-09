878b7826ed806f95ac8a71952455d1d4f9df21a7
karan100010 <karanjoshi1993@gmail.com>
2024-05-09 10:55:56 +0530

    added

--- connection.py
+++ connection.py
@@ -130,7 +130,7 @@ class Connection:
         combined_audio += audio
       return combined_audio
     except Empty:
-      return bytes(1600)
+      return bytes(640)
     
 
     if self._asterisk_resample:
