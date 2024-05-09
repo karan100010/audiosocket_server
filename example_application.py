b5aa298e5bce7d20a807be7614d2244b8a03d072
karan100010 <karanjoshi1993@gmail.com>
2024-05-09 11:09:04 +0530

    add

--- example_application.py
+++ example_application.py
@@ -327,7 +327,7 @@ class AudioStreamer():
                     elif self.call_flow["main_audios"][self.intent+"_"+str(self.level)][self.flow_num][1]["meta"]=="hangup":
                         if self.call_flow["main_audios"][self.intent+"_"+str(self.level)][self.flow_num][1]["silence"]:
                             self.long_silence=0
-                            while self.long_silence<50:
+                            while self.long_silence<10:
                                 if self.call.connected:
                                     sleep(.2)
                                 else:
@@ -382,7 +382,7 @@ class AudioStreamer():
                 
 
                 self.long_silence=0    
-                while self.long_silence<50:
+                while self.long_silence<10:
                 #self.logger.info("waiting for silence")
                     if self.call.connected:
                         sleep(.2)
