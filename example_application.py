b27d972c357e4af31c949b6ba24383c29e46c9c9
karan100010 <karanjoshi1993@gmail.com>
2024-05-23 11:18:26 +0530

    added

--- example_application.py
+++ example_application.py
@@ -288,7 +288,11 @@ class AudioStreamer():
                           "call_id": self.call_id
                           }
         try:
-            x = self.conn["ats_tool"]["audios"].insert_one(database_entry)
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            create_intent = requests.post(
+                "http://172.16.1.209:5000/api/audios", json=database_entry, headers=headers)
             self.logger.info("data inserted into db")
             self.logger.info(x)
         except Exception as e:
