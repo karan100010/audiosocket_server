e3047af9097a60e714ff2d956742e68305f56c6e
karan100010 <karanjoshi1993@gmail.com>
2024-06-08 18:31:02 +0530

    added

--- example_application.py
+++ example_application.py
@@ -715,8 +715,8 @@ async def handel_call():
  #   loop = asyncio.get_event_loop()
     while True:
         with ThreadPoolExecutor(max_workers=7) as executor:
-             #       call_list.append(loop.run_in_executor(executor,start_call_fn,audiosocket))
-               await asyncio.to_thread(executor.submit, start_call_fn, audiosocket)
+               call_list.append(loop.run_in_executor(executor,start_call_fn,audiosocket))
+               #await asyncio.to_thread(executor.submit, start_call_fn, audiosocket)
                     
 async def main():
     await handel_call()
