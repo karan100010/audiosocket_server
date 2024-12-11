6dbd9089a4e54255c3d4ee8cff4fbed1d6477a9e
karan100010 <karanjoshi1993@gmail.com>
2024-12-11 19:31:19 +0530

    added

--- audiosocket.py
+++ audiosocket.py
@@ -20,15 +20,11 @@ class audioop_struct:
 
 
 # Creates a new audiosocket object
-import socket
-from threading import Thread
-from time import sleep
-
 class Audiosocket:
     def __init__(self, bind_info, timeout=None):
 
         # By default, features of audioop (for example: resampling
-        # or remixing input/output) are disabled
+        # or re-mixng input/output) are disabled
         self.user_resample = None
         self.asterisk_resample = None
 
@@ -41,10 +37,10 @@ class Audiosocket:
         self.initial_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.initial_sock.bind((self.addr, self.port))
         self.initial_sock.settimeout(timeout)
-        self.initial_sock.listen(5)  # Allows up to 5 pending connections
+        self.initial_sock.listen(1)
 
         # If the user didn't specify a port, the one that the operating system
-        # chose is available in this attribute
+        # chose is availble in this attribute
         self.port = self.initial_sock.getsockname()[1]
 
     # Optionally prepares audio sent by the user to
@@ -75,24 +71,20 @@ class Audiosocket:
 
     def listen(self):
         print('Listening on', self.addr, self.port)
+        conn, peer_addr = self.initial_sock.accept()
+        connection = Connection(
+            conn,
+            peer_addr,
+            self.user_resample,
+            self.asterisk_resample,
+        )
+        
+        
+        connection_thread = Thread(target=connection._process, args=())
+        connection_thread.start()
+        sleep(.1)
 
-        def handle_connection(conn, peer_addr):
-            connection = Connection(
-                conn,
-                peer_addr,
-                self.user_resample,
-                self.asterisk_resample,
-            )
-            connection._process()
+        return connection
 
-        while True:
-            try:
-                conn, peer_addr = self.initial_sock.accept()
-                print(f"Accepted connection from {peer_addr}")
-                connection_thread = Thread(target=handle_connection, args=(conn, peer_addr))
-                connection_thread.start()
-                sleep(0.1)
-            except socket.timeout:
-                continue
-            except Exception as e:
-                print(f"Error accepting connection: {e}")
+        # *** If we want this single object to serve multiple simultaneous connections, accept() will have to be put in a while loop
+        # If this does become the case, what is the best way to deliver the queue objects to the caller, keep them wrapped in read/write methods?
