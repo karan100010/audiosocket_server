3540022bac5bc402058814495d4ca313ef6e7976
karan100010 <karanjoshi1993@gmail.com>
2024-12-12 22:19:56 +0530

    added

--- example_application.py
+++ example_application.py
@@ -382,7 +382,7 @@ class AudioStreamer():
                     
                 #     # Print the response received from the WebSocket
                     response = ws.recv()
-                    print("Vosk Response:", response["partial"])
+                    print("Vosk Response:", json.loads(response["partial"]))
             except websocket.WebSocketException as e:
                 self.logger.error("Failed to connect to Vosk WebSocket: %s", e)        
 
