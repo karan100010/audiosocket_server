a6e331479d6a1f4791856b38a3ddd4e137b6e9bb
karan100010 <karanjoshi1993@gmail.com>
2024-12-16 10:23:48 +0530

    added

--- prompt.py
+++ prompt.py
@@ -27,39 +27,7 @@ Follow the given steps:
      - If payment is not resolved: "Thank you for speaking with me. Please contact us if you need further assistance. Have a good day!"  
 
 Always maintain a calm and respectful tone throughout the conversation.
-Follow the example given below:
 
-Example 1:
-
-Assistent: Hello is this Karan Joshi?
-User:Yes
-Assistent: You have a payment of due would you like to pay now?
-User: Yes
-Assistent: How would you like to make the payment?
-User:Credit card
-Assistent: Send you the link please make the payment.<end>
-
-Example 2:
-
-Assistent: Hello is this Karan Joshi?
-User:NO
-Assistent: Can you connect me to Karan right now?
-User: No
-Assistent: When can I call back to talk to Karan?
-User:Tommorow
-Assistent: Let me call tommorow have a great day.<end>
-
-Example 3:
-
-Assistent: Hello is this Karan Joshi?
-User:jfaskjdf hfaksjf
-Assistent: Not able to understand what you said let me connect you to my adivser?<transfer>
-
-Example 4:
-
-Assistent: Hello is this Karan Joshi?
-User:No, this is a wrong number
-Assistent: Sorry, will not call you again. And we are going to block this number <end>
 
 
 
@@ -123,3 +91,37 @@ def simulate_conversation():
 
 
 simulate_conversation()
+
+"""Follow the example given below:
+
+Example 1:
+
+Assistent: Hello is this Karan Joshi?
+User:Yes
+Assistent: You have a payment of due would you like to pay now?
+User: Yes
+Assistent: How would you like to make the payment?
+User:Credit card
+Assistent: Send you the link please make the payment.<end>
+
+Example 2:
+
+Assistent: Hello is this Karan Joshi?
+User:NO
+Assistent: Can you connect me to Karan right now?
+User: No
+Assistent: When can I call back to talk to Karan?
+User:Tommorow
+Assistent: Let me call tommorow have a great day.<end>
+
+Example 3:
+
+Assistent: Hello is this Karan Joshi?
+User:jfaskjdf hfaksjf
+Assistent: Not able to understand what you said let me connect you to my adivser?<transfer>
+
+Example 4:
+
+Assistent: Hello is this Karan Joshi?
+User:No, this is a wrong number
+Assistent: Sorry, will not call you again. And we are going to block this number <end>"""
