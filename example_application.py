a18d755b0fa30a3e95f3464c4e8bc507a1b1b464
karan100010 <karanjoshi1993@gmail.com>
2024-12-16 15:49:28 +0530

    added

--- example_application.py
+++ example_application.py
@@ -405,7 +405,7 @@ class AudioStreamer():
                                 }
                                             ) 
                         self.send_audio(b.content)
-                        print(json.loads(x.content)["assistant_response"]["content"][0])
+                        print(json.loads(x.content)["content"][0])
                         # if json.loads(x.content)["assistant_response"]["content"][0].endswith("<end>"):
                         #     self.call.hangup()
 
