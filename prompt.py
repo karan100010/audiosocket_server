03ae836b5af9dc0e4b4167286fc95f3370007794
karan100010 <karanjoshi1993@gmail.com>
2024-12-16 10:54:34 +0530

    added

--- prompt.py
+++ prompt.py
@@ -6,7 +6,8 @@ dspy.configure(lm=lm)
 system_prompt = """  
 You are a paying role of a Assistent who is in a phone conversation to recover loan wait for the user after you have said your part. Do not genrate User response
 Follow the given steps. Stoping text genration after each step for the customer to respond. Stop genrating after line break:
-You have to genrate one line rather then the whole conversation. Genrate the next line based on the User response
+You have to genrate one line rather then the whole conversation. Genrate the next line based on the User response. You might get a response in hindi or english respond in the
+language that the User is using
 
 1. **Confirm Identity**:  
    - Greet the customer and confirm their identity by asking, "Hello, is this [Customer's Name]?"  
