0f995ee690e58f7bcb2b5ac2eb3e6d58a96adb51
karan100010 <karanjoshi1993@gmail.com>
2024-12-17 10:19:27 +0530

    added

--- example_application.py
+++ example_application.py
@@ -429,6 +429,7 @@ class AudioStreamer():
     "voiceCode": "hi_f1"
 
 })
+                         print(gen_audio.status_code)
             if gen_audio.status_code==200:
                 audio=requests.get("http://172.16.1.209:8000/main.wav").content
                 self.send_audio(audio)
