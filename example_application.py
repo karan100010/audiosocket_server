12b60fcf6717c42ffec53e1d28c4ff24941f97cf
karan100010 <karanjoshi1993@gmail.com>
2024-12-17 10:32:08 +0530

    added

--- example_application.py
+++ example_application.py
@@ -430,10 +430,10 @@ class AudioStreamer():
 
 })
                          print(gen_audio.status_code)
-            if gen_audio.status_code==200:
-                audio=requests.get("http://172.16.1.209:8000/main.wav").content
-                self.send_audio(audio)
-                sleep(5)
+                         if gen_audio.status_code==200:
+                            audio=requests.get("http://172.16.1.209:8000/main.wav").content
+                            self.send_audio(audio)
+                            sleep(5)
               
         # else:
             
